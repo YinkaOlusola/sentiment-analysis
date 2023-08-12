71bcf6f39a98a129d7bb33f3cbf9c795a5cf6733
YinkaAkindele <akindeleyinka1005@gmail.com>
2023-08-12 12:53:16 +0100

    resolving dataset file error

--- Streamlit/base_app.py
+++ Streamlit/base_app.py
@@ -32,7 +32,7 @@ from nltk.corpus import stopwords, wordnet
 
 
 # Load your raw data
-raw = pd.read_csv("Dataset/train")
+raw = pd.read_csv("Dataset/train.csv")
 
 # The main function where we will build the actual app
 def main():
